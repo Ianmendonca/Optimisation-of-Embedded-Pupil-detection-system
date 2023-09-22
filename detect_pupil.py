100d8008cf011754009dd117d04e7aafb82ef021
Ianmendonca <97366497+Ianmendonca@users.noreply.github.com>
2023-09-22 13:34:39 +0200

    Update detect_pupil.py
    comments about thresholding added and extra space were removed

--- detect_pupil.py
+++ detect_pupil.py
@@ -1,5 +1,3 @@
-# show both camera streams 
-
 import cv2
 import numpy as np
 import random as rng
@@ -36,13 +34,12 @@ def draw_ellipse(_drawing, _contours_filtered):
         except TypeError:
             pass
     return _drawing
-
-
+	
 srcPiCam = 'libcamerasrc ! video/x-raw,width=640,height=480 ! videoflip method=clockwise ! videoconvert ! appsink drop=True'
 pcap = cv2.VideoCapture(srcPiCam)
+
 if pcap.isOpened():
         print(f'Puil camera available:')
-
 while True:
         pret, pframe = pcap.read()
         if pret:                
@@ -50,14 +47,22 @@ while True:
                 output = pframe.copy()
                 gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
                 blurred = cv2.GaussianBlur(gray, (3,3), 0)
-                _, thresholded = cv2.threshold(blurred, 124, 255, cv2.THRESH_BINARY_INV)
+		
+		''' Image binarisation/thresholding divides the eye image into two parts,
+		    black(pupil) and white(rest of the eye).
+      		    _, thresholded = cv2.threshold(X, Y, Z, cv2.THRESH_BINARY_INV)#new method added
+		    X is the output of previous operation, Y= thresholding pixel value which has to be 
+  		   adjusted according the condition, Y is the maximum value of pixel'''
+		
+                _, thresholded = cv2.threshold(blurred, 124, 255, cv2.THRESH_BINARY_INV)#new method added
                 contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 		contours_filtered = filter_contour(contours)
                 drawing = np.zeros((blurred.shape[0], blurred.shape[1], 3), dtype=np.uint8)
                 drawing = draw_ellipse(blurred, contours_filtered)
                 cv2.imshow('dframe', drawing  )
+		
         if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-
+		
 pcap.release()
 cv2.destroyAllWindows()
